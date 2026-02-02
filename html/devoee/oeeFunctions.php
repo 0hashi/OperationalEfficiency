1b1f408a987fc8bbfc684b456534a9ece00dedba
0hashi <pohashi@gmail.com>
2026-02-02 11:15:56 -0600

    Update

--- html/devoee/oeeFunctions.php
+++ html/devoee/oeeFunctions.php
@@ -4,10 +4,10 @@ Paul Ohashi
 oeeFunctions.php - Function for connecting to, and pulling data from Rubicon's MariaDG:
 */
 function connectRubiconTci() {
-    $host = 'rubicon.transcableusa.com';
-    $user = 'paul';
-    $pass = 'lNpFSa9S4j5rqOJ';
-    $db   = 'tci';
+    $host = '';
+    $user = '';
+    $pass = '';
+    $db   = '';
 
     $conn = new mysqli($host, $user, $pass, $db);
 
